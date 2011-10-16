4e6bea9ae5f55bac220b8120648f1d8248d34e54
Tomas Neme <lacrymology@gmail.com>
2011-10-16 00:10:15 -0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='image_gallery',
+    version='0.0.0',
+    author='Tomas Neme',
+    author_email='lacrymology@gmail.com',
+    url='http://github.com/Lacrymology',
+    description = 'Django generic image gallery plugin with support for '
+                  'reordering in admin.'
+    packages=find_packages(),
+    provides=['image_gallery', ],
+    include_package_data=True,
+    requires = [
+        'django-inline-ordering',
+        'sorl-thumbnails',
+        ],
+    package_data={
+        'image_gallery': [
+            'templates/*',
+            'static/*',
+        ]
+    },
+)
