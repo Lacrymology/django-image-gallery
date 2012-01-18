bb280e4ae9f8d37711fee96084f8a1fa0a5aa98e
Tomas Neme <lacrymology@gmail.com>
2012-01-18 00:59:16 -0200

    adding docstrings

--- image_gallery/templatetags/gallery_tags.py
+++ image_gallery/templatetags/gallery_tags.py
@@ -1,13 +1,25 @@
+"""
+Template tags for image-gallery.
+
+ * image_gallery renders the gallery template
+"""
 from django import template
-from django.conf import settings
 
 register = template.Library()
 
 def image_gallery(context):
+    """
+    Renders the template image_gallery/gallery.html passing it the
+    following variables:
+      * gallery: the current `gallery` context variable, which is the gallery
+         to be rendered
+      * context: the parent context, in case anything is needed from it
+
+    TODO: pass the gallery as a parameter
+    """
     gallery = context.get('gallery', None)
     return {
         'gallery': gallery,
-        'media_url' : settings.MEDIA_URL,
         'context': context,
         }
 
